b5a79a27cec00c3339457fc10f537518b883d9a8
a73heredia <aleheredia@outlook.com>
2023-01-20 10:41:46 -0300

    cuarto

--- index.js
+++ index.js
@@ -21,16 +21,24 @@ class ProductManager{
 
     static id = 0;
 
-     addProduct = async (product) => {
+     addProduct = (product) => {
       if(!product.name || !product.price || !product.code || !product.description || !product.price || !product.thumbnail || !product.stock){
         console.log('incompleto');
-      }else{
+      }else {
         product.id = ProductManager.id;
-        this.products.push(product);
         ProductManager.id = ProductManager.id +1;       
-        }
-        return this.products;        
-    }
+      }
+
+      if(this.products.some(ptos => ptos.code === product.code)){
+        console.log('El producto ya existe')
+      }else{
+        this.products.push(product);
+      }
+      
+        // this.products.push(product);
+        return this.products;
+      }    
+    
 
     getProducts = () => {
       return this.products
@@ -41,17 +49,17 @@ class ProductManager{
       if(prod.length > 0){
         return prod;
       }
-      console.log('NOt Found');
+      return 'Not Found';
     }
-    
-}
+  }
+
 
 var pto = new ProductManager();
-pto.addProduct({name: 'ale', price: 99, code: 'abc123', description: 'asdsadasdsad', price: 34, thumbnail: 'www.nose.com/img1', stock: 3});
+//pto.addProduct({ name: 'ale343',price: 99, code: 'abc123', description: 'asdsadasdsad', price: 34, thumbnail: 'www.nose.com/img1', stock: 3});
 
-pto.addProduct({name: 'ale1', price: 199, code: 'abc124', description: 'asdsadasdsad', price: 35, thumbnail: 'www.nose.com/img2', stock: 3});
+pto.addProduct({name: 'ale1', price: 199, code: 'abc123', description: 'asdsadasdsad', price: 35, thumbnail: 'www.nose.com/img2', stock: 3});
 
-pto.addProduct({name: 'ale3', price: 299, code: 'abc125', description: 'asdsadasdsad', price: 36, thumbnail: 'www.nose.com/img3', stock: 3});
+ //pto.addProduct({name: 'ale3', price: 299, code: 'abc125', description: 'asdsadasdsad', price: 36, thumbnail: 'www.nose.com/img3', stock: 3});
  
-//console.log(pto.getProducts());
- console.log(pto.getProductById(7));
+ console.log(pto.getProducts());
+//console.log(pto.getProductById(2));
